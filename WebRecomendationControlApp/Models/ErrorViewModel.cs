12e333a671a64cd6899f9f5a0eded4e12f13116b
Irina <irishka.klimovich.2002@mail.ru>
2022-02-26 13:57:02 +0300

    initial commit

---
+++ WebRecomendationControlApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebRecomendationControlApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
